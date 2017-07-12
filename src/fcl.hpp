cf30398a01c029801dcc8195ff8a423888f11607
szczerbi <szczerbi@thinkpadx250.home>
2017-07-12 21:11:47 +0200

    Updated fcl api, seems fine now

--- src/fcl.hpp
+++ src/fcl.hpp
@@ -13,7 +13,7 @@ namespace detail
 	{
 		success,
 		not_implemented
-	}
+	};
 } // namespace detail
 
 	using error_code = detail::error_code;
@@ -22,29 +22,161 @@ namespace detail
 	using NodeHandle = std::string;
 	using ConnectionHandle = std::string;
 
-	bool load(const std::string&, error_code&);
-	bool write(std::string&, error_code&);
+	/*
+	** 	FILES READ/WRITE
+	*/
 
+	/* 	
+	** 	Loads program from file.
+	*/
+	bool load(std::string path, error_code& ec);
+
+	/* 
+	** 	Writes current program to file.
+	*/
+	bool write(std::string path, error_code& ec);
+
+
+	/*
+	**	FUNCTIONS
+	*/
+
+	/*
+	**  Returns handle to all availble functions.
+	*/
 	std::vector<FunctionHandle> get_functions();
-	std::string get_function_pretty_name(const FunctionHandle&, error_code&);
-	std::vector<TypeHandle> get_return_types(const FunctionHandle&, error_code&);
-	std::vector<TypeHandle> get_arg_types(const FunctionHandle&, error_code&);
 
+	/*
+	**	Returns human readable name of function.
+	**  Throws error on invalid function handle.
+	*/
+	std::string get_function_pretty_name(FunctionHandle hdl, error_code& ec);
+
+	/*
+	**  Returns handles to return types of function.
+	**  Throws error on invalid function handle.
+	*/ 
+	std::vector<TypeHandle> get_return_types(FunctionHandle hdl, error_code& ec);
+
+	/*
+	**  Returns handles to argument types of function.
+	**  Throws error on invalid function handle.
+	*/
+	std::vector<TypeHandle> get_arg_types(FunctionHandle hdl, error_code& ec);
+
+
+	/*
+	**	TYPES
+	*/
+
+	/*
+	**	Returns human readable name of type.
+	**	Throws error on invalid handle.
+	*/
+	std::string get_type_pretty_name(TypeHandle hdl, error_code& ec);
+
+
+	/*
+	**	NODES
+	*/
+
+	/*
+	**  Returns handles to all availble nodes.
+	*/
 	std::vector<NodeHandle> get_nodes();
-	std::vector<ConnectionHandle> get_links(const NodeHandle&, error_code&);
-	std::string get_node_pretty_name(const NodeHandle&, error_code&);
-	FunctionHandle get_node_function(const NodeHandle&, error_code&);
 
-	NodeHandle new_node(const FunctionHandle&, error_code&);
+	/*
+	**	Returns handles to all connections that end as args in @param hdl.
+	**	Throws error on invalid handle.
+	** 	Note: Calling this function for all nodes gives you unique list of all connections.
+	*/
+	std::vector<ConnectionHandle> get_arg_connections(NodeHandle hdl, error_code& ec);
 
-	template<typename T> T get_return(const NodeHandle&, unsigned);
+	/*
+	**	Returns human readable name of the node.
+	**  Throws error on invalid handle.
+	*/
+	std::string get_node_pretty_name(NodeHandle hdl, error_code& ec);
 
-	std::string get_type_pretty_name(const TypeHandle&);
+	/*
+	**  Returns handle to node's function.
+	**	Throws error on invalid handle.
+	*/
+	FunctionHandle get_node_function(NodeHandle hdl, error_code& ec);
 
-	unsigned get_argument_index(const ConnectionHandle&);
-	NodeHandle get_argument_node(const ConnectionHandle&);
+	/*
+	**	Creates function with @param hdl as node's function, then
+	** 	Returns handle to newly created node.
+	**	Throws error on invalid handle.
+	*/
+	NodeHandle create_node(FunctionHandle hdl, error_code& ec);
 
-	ConnectionHandle new_link(const NodeHandle&, unsigned);
+	/*
+	** 	Deleted node.
+	**	Returns true on succesfull delete, returns false on unsuccesfull delete.
+	**	Throws error on invalid handle.
+	*/
+	bool delete_node(NodeHandle hdl, error_code& ec);
+
+
+	/*
+	**  CONNECTIONS
+	*/
+
+	/*
+	**  Returns index of argument at connection arg endpoint or -1 on error.
+	**  Throws error on invalid hdl.
+	*/
+	int get_argument_index(ConnectionHandle hdl);
+
+	/*
+	**	Returns handle to node at connection arg endpoint.
+	**	Throws error on invalid hdl.
+	*/
+	NodeHandle get_argument_node(ConnectionHandle hdl);
+
+	/*
+	**  Returns index of return at connection return endpoint or -1 on error.
+	**  Throws error on invalid hdl.
+	*/
+	int  get_return_index(ConnectionHandle hdl);
+
+	/*
+	**	Returns handle to node at connection return endpoint.
+	**	Throws error on invalid hdl.
+	*/
+	NodeHandle get_return_node(ConnectionHandle hdl);
+
+	/*
+	**	Creates connection with @param return_hdl, @param return_index as return endpoint
+	** 	and @param arg_hdl, @param arg_index as argument endpoint
+	**  Returns handle to this connection.
+	**	Throws error on invalid handle or index
+	*/
+	ConnectionHandle create_connection(NodeHandle return_hdl, int return_index,
+									   NodeHandle arg_hdl, int arg_index,
+									   error_code& ec);
+
+	/*
+	** 	Deleted connection.
+	**	Returns true on succesfull delete, returns false on unsuccesfull delete.
+	**	Throws error on invalid handle.
+	*/
+	bool delete_connection(ConnectionHandle hdl, error_code& ec);
+
+
+	/*
+	**	OUTPUT
+	*/
+
+	/*
+	** 	Returns return of node @param hdl at @param index, or nullptr if error occured.
+	** 	Throws error on:
+	** 		- invalid handle 
+	** 		- invalid index
+	**		- unable to cast to type T
+	*/
+	template<typename T> T* get_return(NodeHandle hdl, int index, error_code& ec);
 
 } // namespace fcl
 
