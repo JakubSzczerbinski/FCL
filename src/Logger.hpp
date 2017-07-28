c3ac21aa3e0e7c32c6479267897dc5f9713ac7d4
szczerbi <vszczerbinski@gmail.com>
2017-07-28 22:52:45 +0200

    Added logger

---
+++ src/Logger.hpp
@@ -0,0 +1,72 @@
+#ifndef LOGGER_HPP
+#define LOGGER_HPP
+
+#include <initializer_list>
+#include <string>
+#include <vector>
+
+#ifndef LOG_LEVEL
+  #define LOG_LEVEL 1
+#endif
+
+#if LOG_LEVEL <= 0
+  #define debug_log(...) log(__FUNCTION__, "DEBUG", __VA_ARGS__)
+#else
+  #define debug_log(...) log()
+#endif
+#if LOG_LEVEL <= 1
+  #define info_log(...) log(__FUNCTION__, "INFO", __VA_ARGS__)
+#else
+  #define info_log(...) log()
+#endif
+#if LOG_LEVEL <= 2
+  #define warn_log(...) log(__FUNCTION__, "WARNING", __VA_ARGS__)
+#else
+  #define warn_log(...) log()
+#endif
+#if LOG_LEVEL <= 3
+  #define error_log(...) log(__FUNCTION__, "ERROR", __VA_ARGS__)
+#else
+  #define error_log(...) log()
+#endif
+
+
+class Logger {
+ public:
+  Logger(const std::initializer_list<std::string>& origin) : origin_{origin} {}
+  Logger(const std::string& origin) : Logger{origin} {}
+  template <typename ... Ms>
+  Logger& log(std::string func_name, std::string log_type, const Ms& ... messages)
+  {
+    fprintf(stderr, "[%s] ", log_type.c_str());
+    for (auto& o : origin_)
+    {
+      fprintf(stderr, "%s/", o.c_str());
+    }
+    fprintf(stderr, " %s: ", func_name.c_str());
+    printLogMessage(messages ...);
+    fprintf(stderr, "\n");
+    return *this;
+  }
+  Logger& log()
+  {
+    return *this;
+  }
+ private:
+  template<typename M, typename ... Ms>
+  Logger& printLogMessage(const M& msg, const Ms& ... messages)
+  {
+    fprintf(stderr, "%s ", msg);
+    printLogMessage(messages ...);
+    return *this;
+  }
+  template <typename M>
+  Logger& printLogMessage(const M& msg)
+  {
+    fprintf(stderr, "%s ", msg);
+    return *this;
+  }
+  std::vector<std::string> origin_;
+};
+
+#endif
